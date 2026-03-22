e7f25596022b99d924558e64b529ea43ce7da9dd
ft-mammoo <mammuofficial007@gmail.com>
2026-03-22 20:54:59 +0530

    Added Post Method in Class Based View

--- api/views.py
+++ api/views.py
@@ -44,4 +44,12 @@ class Employees(APIView):
     def get(self, request):
         employees = Employee.objects.all()
         serializer = EmployeeSerializer(employees, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)   
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
