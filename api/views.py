ff01b26662ca71e35e92f519a6dc68006c15d102
ft-mammoo <mammuofficial007@gmail.com>
2026-03-22 21:08:30 +0530

    `Added EmployeeDetail APIView`

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from django.http import Http404
 
 @api_view(['GET', 'POST'])
 def studentsView(request):
@@ -52,4 +53,11 @@ class Employees(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         print(serializer.errors)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class EmployeeDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            raise Http404
