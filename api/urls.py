f94ded41fc3d49018f199d96463306c9af0be53b
ft-mammoo <mammuofficial007@gmail.com>
2026-03-23 23:23:53 +0530

    `Updated API URLs to use class-based views`

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path ('students/', views.studentsView),
-    path('students/<int:pk>/', views.studentDetailView),
+    #Function based views
+    # path ('studentsFBV/', views.studentsView),
+    # path('studentsFBV/<int:pk>/', views.studentDetailView),
+
+    path('students/', views.Students.as_view()),
+    path('students/<int:pk>/', views.StudentDetail.as_view()),
 
     path('employees/', views.Employees.as_view()),
     path('employees/<int:pk>/', views.EmployeeDetail.as_view()),
