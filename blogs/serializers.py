b09b32fcb0bf03035d0b239d19752511b9a0c266
ft-mammoo <mammuofficial007@gmail.com>
2026-03-26 22:37:07 +0530

    `Update serializers.py to change fields in CommentSerializer and BlogSerializer`

--- blogs/serializers.py
+++ blogs/serializers.py
@@ -4,10 +4,10 @@ from .models import Blog, Comment
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ['id', 'blog', 'comment']
 
 class BlogSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
     class Meta:
         model = Blog
-        fields = '__all__'
+        fields = ['id', 'title', 'content', 'author', 'comments']
