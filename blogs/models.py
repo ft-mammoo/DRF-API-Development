693c65cf4f99dc305398898091673e68d5c9e62c
ft-mammoo <mammuofficial007@gmail.com>
2026-03-26 21:52:13 +0530

    `Added Comment model, admin registration and Made Migrations`

--- blogs/models.py
+++ blogs/models.py
@@ -8,3 +8,12 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, related_name='comments', on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.comment
+
