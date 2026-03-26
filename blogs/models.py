e8cc9e4059c2a535156b5e7b7fab1674346c8d7b
ft-mammoo <mammuofficial007@gmail.com>
2026-03-26 22:28:40 +0530

    `Removed Created_at field from Blog and Comment`

--- blogs/models.py
+++ blogs/models.py
@@ -4,7 +4,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     author = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
@@ -12,7 +11,6 @@ class Blog(models.Model):
 class Comment(models.Model):
     blog = models.ForeignKey(Blog, related_name='comments', on_delete=models.CASCADE)
     comment = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.comment
