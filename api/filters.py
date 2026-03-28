b3b7332c9a254b3b232d2dbe2fad2bdaa61cabdd
ft-mammoo <mammuofficial007@gmail.com>
2026-03-29 00:13:34 +0530

    `Refactor EmployeeFilter in api/filters.py`

--- api/filters.py
+++ api/filters.py
@@ -2,13 +2,13 @@ import django_filters
 from employees.models import Employee
 
 class EmployeeFilter(django_filters.FilterSet):
-    emp_id_min = django_filters.CharFilter(method='filter_emp_id', label='From Emp_id')
-    emp_id_max = django_filters.CharFilter(method='filter_emp_id', label='To Emp_id')
+    emp_id_min = django_filters.CharFilter(field_name='emp_id', lookup_expr='gte')
+    emp_id_max = django_filters.CharFilter(field_name='emp_id', lookup_expr='lte')
     emp_name = django_filters.CharFilter(field_name='emp_name', lookup_expr='icontains')
     designation = django_filters.CharFilter(field_name='designation', lookup_expr='iexact')
     class Meta:
         model = Employee
-        fields = ['emp_id_min', 'emp_id_max', 'emp_name', 'designation']
+        fields = ['emp_name', 'designation']
 
     def filter_emp_id(self, queryset, name, value):
         if name == 'emp_id_min':
