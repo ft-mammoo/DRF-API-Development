58885ccd98ae15ecb28d566ebf114474a3ebf179
ft-mammoo <mammuofficial007@gmail.com>
2026-03-29 00:30:52 +0530

    `Removed filter_emp_id method and updated EmployeeFilter class`

--- api/filters.py
+++ api/filters.py
@@ -6,13 +6,7 @@ class EmployeeFilter(django_filters.FilterSet):
     emp_id_max = django_filters.CharFilter(field_name='emp_id', lookup_expr='lte')
     emp_name = django_filters.CharFilter(field_name='emp_name', lookup_expr='icontains')
     designation = django_filters.CharFilter(field_name='designation', lookup_expr='iexact')
+
     class Meta:
         model = Employee
         fields = ['emp_name', 'designation']
-
-    def filter_emp_id(self, queryset, name, value):
-        if name == 'emp_id_min':
-            return queryset.filter(emp_id__gte=value)
-        elif name == 'emp_id_max':
-            return queryset.filter(emp_id__lte=value)
-        return queryset.filter(emp_id=value)
